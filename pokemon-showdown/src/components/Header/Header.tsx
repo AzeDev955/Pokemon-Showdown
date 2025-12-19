ec0dc86a8f158cfbacf2ca38f75b4f106d96d659
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-12-19 21:49:04 +0100

    feat: header styles

--- pokemon-showdown/src/components/Header/Header.tsx
+++ pokemon-showdown/src/components/Header/Header.tsx
@@ -1,31 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import icono from "../../assets/icono.png";
+import icono from "../../assets/iconotransparente.png";
 import styles from "./Header.module.scss";
 
 export const Header = () => {
   return (
     <header className={styles.container_header}>
-      <>
+      <div className={styles.logo_wrapper}>
         <Link to="/">
-          <img src={icono} alt="logo pokemon showdown" className={styles.img} />
+          <img src={icono} alt="logo pokemon showdown" />
         </Link>
-      </>
-      <nav>
-        <ul className={styles.ul}>
-          <li>
-            <Link to="/pokedex">Pokédex</Link>
-          </li>
-          <li>
-            <Link to="/equipo">Crear equipo</Link>
-          </li>
-          <li>
-            <Link to="/jugar">Jugar contra la maquina</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
+      </div>
+      <nav className={styles.nav}>
+        <Link to="/pokedex">Pokédex</Link>
+        <Link to="/equipo">Crear equipo</Link>
+        <Link to="/jugar">Jugar contra la maquina</Link>
+        <Link to="/login">Login</Link>
       </nav>
     </header>
   );
